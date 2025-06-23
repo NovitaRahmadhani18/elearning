c8cf0a322acd95391140a3512e7f112aaed9bc57
NovitaRahmadhani18 <novitarahmadhani79@gmail.com>
2025-06-23 22:54:04 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -7,27 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Admin Routes
-Route::middleware(['auth', 'verified', 'role:admin'])->prefix('admin')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('pages.admin.dashboard');
-    })->name('admin.dashboard');
-});
-
-// Teacher Routes
-Route::middleware(['auth', 'verified', 'role:teacher'])->prefix('teacher')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('pages.teacher.dashboard');
-    })->name('teacher.dashboard');
-});
-
-// Student Routes
-Route::middleware(['auth', 'verified', 'role:user'])->prefix('student')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('pages.user.dashboard');
-    })->name('student.dashboard');
-});
-
 Route::get('/dashboard', function () {
     if (auth()->user()->hasRole('admin')) {
         return redirect()->route('admin.dashboard');
