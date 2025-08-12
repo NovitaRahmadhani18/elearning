c7cb63aae161e059c7e7377487bbde22a6e7efa7
NovitaRahmadhani18 <novitarahmadhani79@gmail.com>
2025-08-12 19:47:32 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,13 @@ import "./bootstrap";
 
 import { Livewire, Alpine } from "../../vendor/livewire/livewire/dist/livewire.esm";
 import Clipboard from "@ryangjchandler/alpine-clipboard";
+import ajax from "@imacrayon/alpine-ajax";
 import morph from "@alpinejs/morph";
 
 window.Alpine = Alpine;
 
 Alpine.plugin(morph);
 Alpine.plugin(Clipboard);
+Alpine.plugin(ajax);
 
 Livewire.start();
