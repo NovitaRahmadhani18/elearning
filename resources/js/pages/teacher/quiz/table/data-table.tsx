157f8d97451fd0accee9267af43e0edb810702aa
NovitaRahmadhani18 <novitarahmadhani79@gmail.com>
2025-08-24 09:44:53 +0700

    update

--- resources/js/pages/teacher/quiz/table/data-table.tsx
+++ resources/js/pages/teacher/quiz/table/data-table.tsx
@@ -1,12 +1,11 @@
 import DataTable from '@/components/data-table/data-table';
-import { mockProgressData } from '@/pages/student/leaderboard/leaderboard';
 import { studentQuizColumn } from './column';
 
 const StudentQuizTable = () => {
     return (
         <div>
             <DataTable
-                data={mockProgressData[1].submissions}
+                data={[]}
                 columns={studentQuizColumn}
                 title="Completed Students"
             />
